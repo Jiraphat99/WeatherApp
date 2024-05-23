d526aeca9ac44b9b89700f7b4ade958cf5d98d5f
Jirapaht <jiraphat99@icloud.com>
2024-05-23 17:20:17 +0200

    Organizing js

--- javascript.js
+++ javascript.js
@@ -49,38 +49,47 @@ function formatDate(date) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("search-form");
-  const searchInput = document.getElementById("search-input");
-  const alert = document.getElementById("alert");
-  const currentCity = document.getElementById("current-city");
-  const currentDate = document.getElementById("current-date");
-  const weatherIcon = document.getElementById("weather-icon");
-  const currentTemperature = document.getElementById("currentTemperature");
-  const currentSky = document.getElementById("currentSky");
-  const currentCountry = document.getElementById("currentCountry");
-  const humidity = document.getElementById("humidity");
-  const windSpeed = document.getElementById("wind-speed");
-  const feelLike = document.getElementById("feelLike");
-  const weatherApp = document.querySelector(".weatherApp");
-  const defaultInfo = document.querySelector(".currentCity-section");
-  const dayInfo = document.querySelector(".day_info");
-  const listContent = document.querySelector(".list_content");
-
   const apiKey = "097tobe889c8b3ef74487a6e720a70b1";
 
-  // Hide weather information elements initially
-  weatherApp.style.visibility = "visible";
-  defaultInfo.classList.add("hidden");
-  dayInfo.classList.add("hidden");
-  listContent.classList.add("hidden");
+  // DOM Elements
+  const elements = {
+    form: document.getElementById("search-form"),
+    searchInput: document.getElementById("search-input"),
+    alert: document.getElementById("alert"),
+    currentCity: document.getElementById("current-city"),
+    currentDate: document.getElementById("current-date"),
+    weatherIcon: document.getElementById("weather-icon"),
+    currentTemperature: document.getElementById("currentTemperature"),
+    currentSky: document.getElementById("currentSky"),
+    currentCountry: document.getElementById("currentCountry"),
+    humidity: document.getElementById("humidity"),
+    windSpeed: document.getElementById("wind-speed"),
+    feelLike: document.getElementById("feelLike"),
+    weatherApp: document.querySelector(".weatherApp"),
+    defaultInfo: document.querySelector(".currentCity-section"),
+    dayInfo: document.querySelector(".day_info"),
+    listContent: document.querySelector(".list_content"),
+  };
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const city = searchInput.value.trim();
+  // Initialize UI
+  initializeUI();
+
+  // Event Listeners
+  elements.form.addEventListener("submit", handleFormSubmit);
+
+  // Functions
+  function initializeUI() {
+    elements.weatherApp.style.visibility = "visible";
+    hideWeatherInfo();
+  }
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    const city = elements.searchInput.value.trim();
     if (city) {
       getWeatherByCity(city);
     }
-  });
+  }
 
   function getWeatherByCity(city) {
     const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
@@ -88,13 +97,14 @@ document.addEventListener("DOMContentLoaded", () => {
       .get(apiUrl)
       .then((response) => {
         updateWeatherInfo(response.data);
-        alert.style.display = "none";
+        elements.alert.style.display = "none";
+        elements.weatherApp.classList.remove("centered"); // Remove centered class on success
       })
       .catch((error) => {
         console.error(error);
-        alert.textContent = "Location not found. Please try again.";
-        alert.style.display = "block";
+        showAlert("Location not found. Please try again.");
         hideWeatherInfo();
+        elements.weatherApp.classList.add("centered"); // Add centered class on error
       });
   }
 
@@ -104,41 +114,52 @@ document.addEventListener("DOMContentLoaded", () => {
       .get(apiUrl)
       .then((response) => {
         updateWeatherInfo(response.data);
-        alert.style.display = "none";
+        elements.alert.style.display = "none";
+        elements.weatherApp.classList.remove("centered"); // Remove centered class on success
       })
       .catch((error) => {
         console.error(error);
-        alert.textContent =
-          "Unable to retrieve weather data. Please search for your city manually.";
-        alert.style.display = "block";
+        showAlert(
+          "Unable to retrieve weather data. Please search for your city manually."
+        );
         hideWeatherInfo();
+        elements.weatherApp.classList.add("centered"); // Add centered class on error
       });
   }
 
   function updateWeatherInfo(data) {
-    currentCity.textContent = data.city;
-    currentCountry.textContent = data.country;
-    currentDate.textContent = new Date().toLocaleString();
-    weatherIcon.src = data.condition.icon_url;
-    currentTemperature.textContent = `${Math.round(
+    elements.currentCity.textContent = data.city;
+    elements.currentCountry.textContent = data.country;
+    elements.currentDate.textContent = new Date().toLocaleString();
+    elements.weatherIcon.src = data.condition.icon_url;
+    elements.currentTemperature.textContent = `${Math.round(
       data.temperature.current
     )}°C`;
-    currentSky.textContent = data.condition.description;
-    humidity.textContent = `${data.temperature.humidity}%`;
-    windSpeed.textContent = `${data.wind.speed} Km/h`;
-    feelLike.textContent = `${data.temperature.feels_like}%`; // Assuming rainy value needs to be updated
+    elements.currentSky.textContent = data.condition.description;
+    elements.humidity.textContent = `${data.temperature.humidity}%`;
+    elements.windSpeed.textContent = `${data.wind.speed} Km/h`;
+    elements.feelLike.textContent = `${Math.round(
+      data.temperature.feels_like
+    )}°C`;
 
-    // Show weather information elements
-    defaultInfo.classList.remove("hidden");
-    dayInfo.classList.remove("hidden");
-    listContent.classList.remove("hidden");
+    showWeatherInfo();
+  }
+
+  function showWeatherInfo() {
+    elements.defaultInfo.classList.remove("hidden");
+    elements.dayInfo.classList.remove("hidden");
+    elements.listContent.classList.remove("hidden");
   }
 
   function hideWeatherInfo() {
-    // Hide weather information elements
-    defaultInfo.classList.add("hidden");
-    dayInfo.classList.add("hidden");
-    listContent.classList.add("hidden");
+    elements.defaultInfo.classList.add("hidden");
+    elements.dayInfo.classList.add("hidden");
+    elements.listContent.classList.add("hidden");
+  }
+
+  function showAlert(message) {
+    elements.alert.textContent = message;
+    elements.alert.style.display = "block";
   }
 
   function getUserLocation() {
@@ -151,17 +172,19 @@ document.addEventListener("DOMContentLoaded", () => {
         },
         (error) => {
           console.error(error);
-          alert.textContent =
-            "Location access denied. Please search for your city manually.";
-          alert.style.display = "block";
+          showAlert(
+            "Location access denied. Please search for your city manually."
+          );
           hideWeatherInfo();
+          elements.weatherApp.classList.add("centered"); // Add centered class on location denied
         }
       );
     } else {
-      alert.textContent =
-        "Geolocation not supported. Please search for your city manually.";
-      alert.style.display = "block";
+      showAlert(
+        "Geolocation not supported. Please search for your city manually."
+      );
       hideWeatherInfo();
+      elements.weatherApp.classList.add("centered"); // Add centered class if geolocation not supported
     }
   }
 
